9b45bd4db73083b6ed2ca8ec98607c09327842fa
dominickdevincenzo <devindm101@gmail.com>
2024-07-10 21:41:37 -0400

    create functionality to process videos locally

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,12 +1,62 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
+import fs from "fs";
 
 const app = express();
-const port = 3000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello word!");
+app.post("/process-video", (req, res) => {
+  // get path of the input video file from the request body
+  const inputFilePath = req.body.inputFilePath;
+  const outputFilePath = req.body.outputFilePath;
+
+  //   check if input file path is defined
+  if (!inputFilePath || !outputFilePath) {
+    res.status(400).send("Bad Request: Missing file path.");
+  }
+
+  const startTime = Date.now();
+
+  //   create ffmpeg command
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-2:360") // scale to 360p
+    .on("start", (commandLine) => {
+      console.log(`Spawned ffmpeg with command: ${commandLine}`);
+    })
+    .on("progress", (progress) => {
+      console.log(
+        `Processing: ${progress.percent ? progress.percent : 0}% done`
+      );
+    })
+    .on("end", function () {
+      const endTime = Date.now();
+      const duration = (endTime - startTime) / 1000;
+      console.log(`Video processing finished in ${duration} seconds`);
+
+      //   check if the output file was created and has size > 0
+      fs.stat(outputFilePath, (err, stats) => {
+        if (err) {
+          console.error(`Error checking output file: ${err.message}`);
+          return res
+            .status(500)
+            .send(`Error checking output file ${err.message}`);
+        }
+        if (stats.size == 0) {
+          console.error("Processed video is 0KB");
+          return res.status(500).send("Processed video is 0KB");
+        }
+        res.status(200).send("Video processing finished successfully.");
+      });
+    })
+    .on("error", function (err, stdout, stderr) {
+      console.log(`An error occurred: ${err.message}`);
+      console.error(`ffmpeg stderr: ${stderr}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
