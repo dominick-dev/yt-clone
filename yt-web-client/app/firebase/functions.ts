53eb8187d3e72e1cac21e688681e051968a9c5e9
dominickdevincenzo <devindm101@gmail.com>
2024-07-12 01:00:06 -0400

    add functionality to show uploaded videos

--- yt-web-client/app/firebase/functions.ts
+++ yt-web-client/app/firebase/functions.ts
@@ -1,23 +1,36 @@
-import { getFunctions, httpsCallable } from "firebase/functions";
-
-const functions = getFunctions();
+import { httpsCallable } from "firebase/functions";
+import { functions } from "./firebase";
 
 const generateUploadUrl = httpsCallable(functions, "generateUploadUrl");
+const getVideosFunction = httpsCallable(functions, "getVideos");
+
+export interface Video {
+  id?: string;
+  uid?: string;
+  filename?: string;
+  status?: "processing" | "processed";
+  title?: string;
+  description?: string;
+}
 
 export async function uploadVideo(file: File) {
-    
-    const response: any = await generateUploadUrl({
-        fileExtension: file.name.split('.').pop()
-    });
+  const response: any = await generateUploadUrl({
+    fileExtension: file.name.split(".").pop(),
+  });
 
-    // upload file via signed URL
-    await fetch(response?.data?.url, {
-        method: "PUT",
-        body: file,
-        headers: {
-            "Content-Type": file.type
-        }
-    });
-    
-    return;
+  // upload file via signed URL
+  await fetch(response?.data?.url, {
+    method: "PUT",
+    body: file,
+    headers: {
+      "Content-Type": file.type,
+    },
+  });
+
+  return;
+}
+
+export async function getVideos() {
+  const response: any = await getVideosFunction();
+  return response.data as Video[];
 }
